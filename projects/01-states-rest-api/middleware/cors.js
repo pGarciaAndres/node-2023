786861f82783d21850b135bf56e9dc46c4184e4e
Pablo Garcia <garciandres.15@gmail.com>
2024-02-13 10:05:27 +0100

    cors

--- projects/01-states-rest-api/middleware/cors.js
+++ projects/01-states-rest-api/middleware/cors.js
@@ -2,19 +2,22 @@ import cors from 'cors'
 
 const ALLOWED_ORIGINS = [
   'http://localhost:3000',
+  'http://localhost:8080',
+  'http://192.168.100.12:8080/',
   'http://states.com'
 ]
 
-export const corsMiddleware = ({ allowedOrigins = ALLOWED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true)
-    }
+export const corsMiddleware = ({ allowedOrigins = ALLOWED_ORIGINS } = {}) =>
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true)
+      }
 
-    if (!origin) {
-      return callback(null, true)
-    }
+      if (!origin) {
+        return callback(null, true)
+      }
 
-    return callback(new Error('Not allowed by CORS'))
-  }
-})
+      return callback(new Error('Not allowed by CORS'))
+    }
+  })
